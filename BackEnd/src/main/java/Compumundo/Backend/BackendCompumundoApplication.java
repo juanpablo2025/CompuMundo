b4e2653cfd4f9ed1da34dea53065548baace03a7
Juan Pablo Avendaño Pino <65789030+juanpablo2025@users.noreply.github.com>
2023-06-01 02:22:53 -0500

    Se crea proyecto Compumundo

---
+++ BackEnd/src/main/java/Compumundo/Backend/BackendCompumundoApplication.java
@@ -0,0 +1,13 @@
+package Compumundo.Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCompumundoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendCompumundoApplication.class, args);
+	}
+
+}
