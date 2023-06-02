bf2477e979b68be2bbdd034dd93b558056fa7f37
Juan Avendano <juanpablo_1810dev@hotmail.com>
2023-06-02 13:50:24 -0500

    Se crea componente Home e interfaces

---
+++ FrontEnd/src/app/interfaces/almacenamiento.ts
@@ -0,0 +1,6 @@
+export interface Almacenamiento{
+    iup:number,
+    zona:string,
+    unidadescubicas:string
+    
+}
---
+++ FrontEnd/src/app/interfaces/mercancia.ts
@@ -0,0 +1,14 @@
+import { Almacenamiento } from "./almacenamiento";
+
+export interface Mercancia {
+
+    iup:number,
+    nombre:string,
+    descripcion:string,
+    fecha:string,
+    motivo:string,
+    volumen:string,
+    almacenamiento:Almacenamiento
+
+
+}
