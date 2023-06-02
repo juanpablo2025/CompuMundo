88ea94bcbd6619a08b8f53ce0201cca312c13fe0
Juan Avendano <juanpablo_1810dev@hotmail.com>
2023-06-02 14:11:54 -0500

    sé queman mercancias

--- FrontEnd/src/app/home/home.component.ts
+++ FrontEnd/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Mercancia } from '../interfaces/mercancia';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +8,54 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  public mercancias:Mercancia[]=[]
+
+  public mercancias1:Mercancia={
+    iup:1,
+    nombre:"aaa",
+    descripcion:"ddddddddd",
+    fecha:"iii",
+    motivo:"eee",
+    volumen:"wewewe",
+    almacenamiento:{
+      iup:1,
+      zona:"B",
+      unidadescubicas:"234324"
+    }
+    
+
+
+
+  } public mercancias2:Mercancia={
+    iup:1,
+    nombre:"aaa",
+    descripcion:"ddddddddd",
+    fecha:"iii",
+    motivo:"eee",
+    volumen:"wewewe",
+    almacenamiento:{
+      iup:1,
+      zona:"B",
+      unidadescubicas:"234324"
+    }
+
+}
+public mercancias3:Mercancia={
+  iup:1,
+  nombre:"aaa",
+  descripcion:"ddddddddd",
+  fecha:"iii",
+  motivo:"eee",
+  volumen:"wewewe",
+  almacenamiento:{
+    iup:1,
+    zona:"B",
+    unidadescubicas:"234324"
+  }}
+public constructor(){
+  this.mercancias.push(this.mercancias1)
+    this.mercancias.push(this.mercancias2)
+    this.mercancias.push(this.mercancias3)
+    console.log(this.mercancias)
+}
 }
