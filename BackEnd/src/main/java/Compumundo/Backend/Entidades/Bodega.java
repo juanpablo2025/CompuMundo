1fb69fbfb4045fd3b580e62d76a7bc683d1d73a8
Juan Avendano <juanpablo_1810dev@hotmail.com>
2023-06-09 12:16:30 -0500

    se actualizan entidades

--- BackEnd/src/main/java/Compumundo/Backend/Entidades/Bodega.java
+++ BackEnd/src/main/java/Compumundo/Backend/Entidades/Bodega.java
@@ -1,2 +1,62 @@
-package Compumundo.Backend.Entidades;public class Bodega {
+package Compumundo.Backend.Entidades;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="bodega")
+public class Bodega {
+
+
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+@Column(name="id")
+private Integer id;
+
+    @Column(name="ControlEntrada",nullable = false, length =50 )
+    private String ControEntrada;
+
+    @Column(name="ControlSalida",nullable = false, length =50 )
+    private String ControlSalida;
+
+    @Column(name="Ubicacion",nullable = false, length =50 )
+    private String Ubicacion;
+
+    public Bodega(Integer id, String controEntrada, String controlSalida, String ubicacion) {
+        this.id = id;
+        ControEntrada = controEntrada;
+        ControlSalida = controlSalida;
+        Ubicacion = ubicacion;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getControEntrada() {
+        return ControEntrada;
+    }
+
+    public void setControEntrada(String controEntrada) {
+        ControEntrada = controEntrada;
+    }
+
+    public String getControlSalida() {
+        return ControlSalida;
+    }
+
+    public void setControlSalida(String controlSalida) {
+        ControlSalida = controlSalida;
+    }
+
+    public String getUbicacion() {
+        return Ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        Ubicacion = ubicacion;
+    }
 }
--- BackEnd/src/main/java/Compumundo/Backend/Entidades/Mercancia.java
+++ BackEnd/src/main/java/Compumundo/Backend/Entidades/Mercancia.java
@@ -1,2 +1,99 @@
-package Compumundo.Backend.Entidades;public class Mercancia {
+package Compumundo.Backend.Entidades;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="estudiantes")
+public class Mercancia {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name="IUP")
+    private Integer id;
+
+    @Column(name="nombre",nullable = false, length =100)
+    private String nombre;
+
+    @Column(name="descripcion",nullable = false, length =100)
+    private String descripcion;
+
+    @Column(name="fecha de entrada",nullable = false, length =50)
+    private String fecha_entrada;
+
+    @Column(name="motivo devolucion",nullable = false, length =100)
+    private String motivo_devolucion;
+
+    @Column(name="volumen",nullable = false, length =50)
+    private String volumen;
+
+    @OneToOne(mappedBy = "estudiante", cascade = CascadeType.ALL)
+    @JsonBackReference
+    private Bodega bodega;
+
+    public Mercancia(Integer id, String nombre, String descripcion, String fecha_entrada, String motivo_devolucion, String volumen, Bodega bodega) {
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.fecha_entrada = fecha_entrada;
+        this.motivo_devolucion = motivo_devolucion;
+        this.volumen = volumen;
+        this.bodega = bodega;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getFecha_entrada() {
+        return fecha_entrada;
+    }
+
+    public void setFecha_entrada(String fecha_entrada) {
+        this.fecha_entrada = fecha_entrada;
+    }
+
+    public String getMotivo_devolucion() {
+        return motivo_devolucion;
+    }
+
+    public void setMotivo_devolucion(String motivo_devolucion) {
+        this.motivo_devolucion = motivo_devolucion;
+    }
+
+    public String getVolumen() {
+        return volumen;
+    }
+
+    public void setVolumen(String volumen) {
+        this.volumen = volumen;
+    }
+
+    public Bodega getBodega() {
+        return bodega;
+    }
+
+    public void setBodega(Bodega bodega) {
+        this.bodega = bodega;
+    }
 }
