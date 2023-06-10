337cf4a02f6b6661b82c4da628fff01ce1654c56
Juan Avendano <juanpablo_1810dev@hotmail.com>
2023-06-09 22:10:59 -0500

    se crean serviciosbodega

--- BackEnd/src/main/java/Compumundo/Backend/Servicios/ServicioBodega.java
+++ BackEnd/src/main/java/Compumundo/Backend/Servicios/ServicioBodega.java
@@ -1,4 +1,79 @@
 package Compumundo.Backend.Servicios;
 
-public class ServicioBodega {
+import Compumundo.Backend.Entidades.Bodega;
+import Compumundo.Backend.Repositorios.RepositorioBodega;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ServicioBodega implements ServicioBase<Bodega>{
+
+    @Autowired
+    protected RepositorioBodega repositorioBodega;
+
+    @Override
+    @Transactional(readOnly=true)
+    public List<Bodega> buscarTodos() throws Exception {
+        try{
+
+            List<Bodega>bodegas =repositorioBodega.findAll();
+            return bodegas;
+            }catch(Exception error){
+                throw new Exception(error.getMessage());
+            }
+
+        }
+
+
+    @Override
+    public Bodega buscarPorId(Integer id) throws Exception {
+        try{
+            Optional<Bodega>bodegaOpcional =repositorioBodega.findById(id);
+            if(bodegaOpcional.isPresent()){
+                return bodegaOpcional.get();
+            }else{
+                throw new Exception("Usuario no encontrado");
+            }
+        }catch(Exception error){
+            throw new Exception(error.getMessage());
+        }
+    }
+
+    @Override
+    public Bodega registrar(Bodega datosARegistrar) throws Exception {
+        try{
+            Bodega bodegaGuardado=repositorioBodega.save(datosARegistrar);
+            return bodegaGuardado;
+        }catch(Exception error){
+            throw new Exception(error.getMessage());
+        }
+    }
+    
+
+    @Override
+    public Bodega actualizar(Integer id, Bodega datosNuevos) throws Exception {
+        return null;
+    }
+
+    @Override
+    public boolean eliminar(Integer id) throws Exception {
+        try{
+
+            Optional<Bodega>acudienteOpcional =repositorioBodega.findById(id);
+            if(acudienteOpcional.isPresent()){
+                repositorioBodega.deleteById(id);
+                return true;
+            }else{
+                throw new Exception("Usuario no encontrado");
+            }
+
+        }catch(Exception error){
+            throw new Exception(error.getMessage());
+        }
+    }
 }
--- BackEnd/src/main/java/Compumundo/Backend/Servicios/ServicioMercancia.java
+++ BackEnd/src/main/java/Compumundo/Backend/Servicios/ServicioMercancia.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ServicioMercancia implements ServicioBase<Mercancia>{
@@ -27,22 +28,67 @@ public class ServicioMercancia implements ServicioBase<Mercancia>{
     }
 
     @Override
+    @Transactional(readOnly=true)
     public Mercancia buscarPorId(Integer id) throws Exception {
-        return null;
+        try{
+            Optional<Mercancia>mercaciaOpcional=repositorioMercancia.findById(id);
+            if(mercaciaOpcional.isPresent()){
+                return mercaciaOpcional.get();
+
+
+            }else{
+                throw new Exception("Usuario no encontrado");
+            }
+        }catch(Exception error){
+            throw new Exception(error.getMessage());
+        }
     }
 
     @Override
     public Mercancia registrar(Mercancia datosARegistrar) throws Exception {
-        return null;
+       try {
+           return repositorioMercancia.save(datosARegistrar);
+       }catch(Exception error){
+           throw new Exception(error.getMessage());
+
+       }
     }
 
     @Override
     public Mercancia actualizar(Integer id, Mercancia datosNuevos) throws Exception {
-        return null;
+       try{
+           Optional<Mercancia>mercanciaOpcional =repositorioMercancia.findById(id);
+           if(mercanciaOpcional.isPresent()){
+               Mercancia mercanciaExistente=mercanciaOpcional.get();
+               mercanciaExistente.setNombre(datosNuevos.getNombre());
+               mercanciaExistente.setDescripcion(datosNuevos.getDescripcion());
+               mercanciaExistente.setFecha_entrada(datosNuevos.getFecha_entrada());
+               mercanciaExistente.setMotivo_devolucion(datosNuevos.getMotivo_devolucion());
+               mercanciaExistente.setVolumen(datosNuevos.getVolumen());
+               Mercancia mercanciaActualizada=repositorioMercancia.save(mercanciaExistente);
+                       return mercanciaActualizada;
+           }else{
+               throw new Exception("Mercancia no encontrada");
+           }
+       }catch(Exception error){
+           throw new Exception(error.getMessage());
+       }
     }
 
     @Override
     public boolean eliminar(Integer id) throws Exception {
-        return false;
+        try{
+
+            Optional<Mercancia>mercanciaOpcional =repositorioMercancia.findById(id);
+            if(mercanciaOpcional.isPresent()){
+                repositorioMercancia.deleteById(id);
+                return true;
+            }else{
+                throw new Exception("Usuario no encontrado");
+            }
+
+        }catch(Exception error){
+            throw new Exception(error.getMessage());
+        }
     }
 }
